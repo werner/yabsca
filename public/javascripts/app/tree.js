963d2c05aaea816be83e74187bf9379cf80b8244
Werner Echezuria <werner_a_e@yahoo.es>
2011-07-09 15:23:24 -0430

    final commit

--- public/javascripts/app/tree.js
+++ public/javascripts/app/tree.js
@@ -6,12 +6,20 @@ Ext.onReady(function(){
       params:{id: measure_id},
       success:function(data){
           var tree=JSON.parse(data.responseText);
-          console.log(tree); 
-          var root_img=paper.image("/images/red_light.png",336,20,64,64);
-          var root=paper.text(400,10,tree.children[0].name);
-          //Ext.get('treecanvas').dom.innerHTML=measure.root[0].measure.name;
+          draw_nodes(tree,400,10);
       } 
     });
+
+    function draw_nodes(tree,x,y){
+        var x1=x;
+        tree.children.map(function(item){
+            var root=paper.text(x1,y,item.name);
+            var root_img=paper.image("/images/"+item.color+"_light.png",x1-32,y+10,64,64);
+            var conect = paper.path("M"+(x1-32)+" "+(x1-32)+" L"+(y+10)+" "+(y+10));
+            x1+=120;
+            draw_nodes(item,x,y+120);
+        });
+    }
 });
 
 
