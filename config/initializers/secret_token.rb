b5c9942cbafe93a6bc02d3765c021b07c578bf97
Werner Echezuria <w.echezuria@360releases.com>
2012-01-11 15:01:33 -0430

    first commit to branch rails3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yabsca32::Application.config.secret_token = 'c2438f47525b456ff99d69feec7e1db69912581e84747e503c824702c4c7e8fcac9cc78e21ac7fd3198b46f874bad12fec66bd5d7b50be2196a9f9a8310fb383'
